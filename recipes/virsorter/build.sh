ec259b7aca8ed3de5d5413f86801beb5703564fa
Nikos Pappas <n.pappas@uu.nl>
2019-09-17 13:52:49 +0200

    make all Steps scripts executable

--- recipes/virsorter/build.sh
+++ recipes/virsorter/build.sh
@@ -8,6 +8,9 @@ sed "s;mga_linux_ia64;mga;" Scripts/Step_first_add_custom_phage_sequence.pl > Sc
 
 ### Reconfiguring compiler for VirSorter Makefile
 cd Scripts/
+# Make all perl scripts executable
+chmod +x Step*
+
 sed 's/gcc/\$\(CC\)/g' Makefile > Makefile.tmp && mv Makefile.tmp Makefile
 
 
@@ -18,9 +21,9 @@ make
 
 ### Organizing executables
 cd ../
-PKG_OUTDIR="${PREFIX}"/share/"${PKG_NAME}"-"${PKG_VERSION}"-"${PKG_BUILDNUM}"/
+PKG_OUTDIR="${PREFIX}"/share/"${PKG_NAME}"-"${PKG_VERSION}"-"${PKG_BUILDNUM}"
 mkdir -pv "${PKG_OUTDIR}"/ "${PREFIX}"/bin/
 cp -r $(ls | grep -v "conda" | grep -v "build") "${PKG_OUTDIR}"/
-ln -s "${PKG_OUTDIR}"/wrapper_phage_contigs_sorter_iPlant.pl "${PREFIX}"/bin/
+ln -s "${PKG_OUTDIR}"/wrapper_phage_contigs_sorter_iPlant.pl "${PREFIX}"/bin
+# The wrapper requires access to the Scripts folder
 ln -s "${PKG_OUTDIR}"/Scripts "${PREFIX}"/bin/
-
